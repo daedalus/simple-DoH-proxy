2c18d7f11db9f98796fc2311f49951bf72344686
dclavijo <clavijodario@gmail.com>
2024-02-01 12:22:42 -0300

    Code Refactor

--- DoHProxy.py
+++ DoHProxy.py
@@ -10,23 +10,20 @@ import urllib.request, urllib.error, urllib.parse
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 53
+urlbase = f"https://{hostname}/dns-query?dns=" 
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
-
 hostname = sys.argv[1]
 gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+
 
 def sslget(url):
     req = urllib.request.Request(url)
     return urllib.request.urlopen(req, context=gcontext).read()
 
-urlbase = f"https://{hostname}/dns-query?dns=" 
 
-###print ssock
-print("Proxy running...")
-
-def DNS_dissect(data,addr):
+def DNS_dissect(data, addr):
     ID = data[:2].encode('hex')
     QR = data[2:4].encode('hex')
     QD = data[4:6].encode('hex')
@@ -34,17 +31,23 @@ def DNS_dissect(data,addr):
     NS = data[8:10].encode('hex')
     AR = data[10:12].encode('hex')
     payload = data[12:].encode('hex')
-    print("ADDR,ID,QR,QD,AN,NS,AR,payload")
-    print(addr,ID,QR,QD,AN,NS,AR,payload)
+    print(f"ADDR: {addr}, ID: {ID}, QR: [QR], QD: {QD}, AN: {AN}, NS: {NS}, AR: {AR}, payload: {payload}")
 
-while True:    
-	data1, addr1 = sock.recvfrom(2048) # buffer size is 1024 bytes
+
+def main():
+    print("Proxy running...")
+    while True:    
+	    data1, addr1 = sock.recvfrom(2048) # buffer size is 1024 bytes
         print("Client request")
-	if len(data1) > 0:
-		DNS_dissect(data1,addr1)
-                url = urlbase + data1.encode("base64").replace("\n","").replace("\r","").replace("=","") 
-                print(url)
-                reply = sslget(url)
-                print("Server reply")
-                DNS_dissect(reply,(hostname,443))
-                sock.sendto(reply,addr1)            
+	    if len(data1) > 0:
+		    DNS_dissect(data1, addr1)
+            url = urlbase + data1.encode("base64").replace("\n","").replace("\r","").replace("=","") 
+            print(url)
+            reply = sslget(url)
+            print("Server reply")
+            DNS_dissect(reply,(hostname,443))
+            sock.sendto(reply,addr1)            
+
+
+if __name__ == "__main__":
+    main()
