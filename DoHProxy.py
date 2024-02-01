289ad9a10fd56aebdfa5f8c66742285827415ef4
dclavijo <clavijodario@gmail.com>
2024-02-01 12:12:12 -0300

    2to3

--- DoHProxy.py
+++ DoHProxy.py
@@ -6,7 +6,7 @@ import socket
 import struct
 import ssl
 import sys
-import urllib2
+import urllib.request, urllib.error, urllib.parse
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 53
@@ -18,13 +18,13 @@ hostname = sys.argv[1]
 gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 
 def sslget(url):
-    req = urllib2.Request(url)
-    return urllib2.urlopen(req, context=gcontext).read()
+    req = urllib.request.Request(url)
+    return urllib.request.urlopen(req, context=gcontext).read()
 
 urlbase = "https://%s/dns-query?dns=" % hostname 
 
 ###print ssock
-print "Proxy running..."
+print("Proxy running...")
 
 def DNS_dissect(data,addr):
 	ID = data[0:2].encode('hex')
@@ -34,17 +34,17 @@ def DNS_dissect(data,addr):
 	NS = data[8:10].encode('hex')
 	AR = data[10:12].encode('hex')
 	payload = data[12:].encode('hex')
-	print "ADDR,ID,QR,QD,AN,NS,AR,payload"
-	print addr,ID,QR,QD,AN,NS,AR,payload
+	print("ADDR,ID,QR,QD,AN,NS,AR,payload")
+	print(addr,ID,QR,QD,AN,NS,AR,payload)
 
 while True:    
 	data1, addr1 = sock.recvfrom(2048) # buffer size is 1024 bytes
-        print "Client request"
+        print("Client request")
 	if len(data1) > 0:
 		DNS_dissect(data1,addr1)
                 url = urlbase + data1.encode("base64").replace("\n","").replace("\r","").replace("=","") 
-                print url
+                print(url)
                 reply = sslget(url)
-                print "Server reply"
+                print("Server reply")
                 DNS_dissect(reply,(hostname,443))
                 sock.sendto(reply,addr1)            
